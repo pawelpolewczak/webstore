7e5a6f9dc2cea2c4de7b7869090f63c1138ce9f0
Pawel Polewczak <pawel.polewczak@capgemini.com>
2017-12-20 17:27:11 +0100

    Add product form and messages resource

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -5,6 +5,9 @@ import com.packt.webstore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -64,8 +67,17 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String processAddNewProductForm(@ModelAttribute("newProduct") Product newProduct){
+    public String processAddNewProductForm(@ModelAttribute("newProduct") Product newProduct, BindingResult result){
+        String[] suppressedFields = result.getSuppressedFields();
+        if(suppressedFields.length > 0){
+            throw new RuntimeException("Proba wiazania niedozwolonych pol: " + StringUtils.arrayToCommaDelimitedString(suppressedFields));
+        }
         productService.addProduct(newProduct);
         return "redirect:/products";
+    }
+
+    @InitBinder
+    public void initialiseBinder(WebDataBinder binder){
+        binder.setDisallowedFields("unitsInOrder", "discontinued");
     }
 }
